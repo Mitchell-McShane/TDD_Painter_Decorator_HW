2cb27ada726a4e268ef1d209653d8779c3f957fd
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-12-08 16:59:43 +0000

    Paint Can second test worked

--- models/paint.js
+++ models/paint.js
@@ -2,4 +2,9 @@ const PaintCan = function (litresOfPaint) {
     this.litresOfPaint = litresOfPaint;
 };
 
+PaintCan.prototype.isEmpty = function () {
+    if (this.litresOfPaint === 0) {
+        return true
+    }
+}
 module.exports = PaintCan;
--- models/specs/paint_spec.js
+++ models/specs/paint_spec.js
@@ -7,10 +7,16 @@ describe('PaintCan', function() {
 
     beforeEach(function() {
         paintCan = new PaintCan(20);
+        paintEmpty = new PaintCan(0);
     });
 
     it('should have a number of litres of paint', function() {
         const litres = 20;
         assert.strictEqual(paintCan.litresOfPaint, litres);
+    });
+
+    it('should be able to check if it is empty', function() {
+        const checkEmpty = paintEmpty.isEmpty();
+        assert.strictEqual(checkEmpty, true)
     });
 })
